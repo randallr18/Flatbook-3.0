bb9c673c2805efc674a248d19ffc141bf0080377
Risher Randall <randallr18@mail.wlu.edu>
2019-01-03 11:16:51 -0600

    Search Bar Update

--- flatbook_frontend/src/components/Projects/ProjectNavigation.js
+++ flatbook_frontend/src/components/Projects/ProjectNavigation.js
@@ -6,22 +6,32 @@ import '../Reviews/ReviewNavigation.css';
 // import { stateOptions } from '../../redux/types';
 
 class ProjectNavigation extends Component {
-  state = { activeItem: this.props.placeholder }
+  state = {
+    activeItem: this.props.placeholder,
+    search_word: ''
+   }
 
   handleItemClick = (e, { name }) => {
     this.setState({ activeItem: name })
   }
 
+  // <Form.Field width={4}>
+  //   <label> Birth Date </label>
+  //   <input placeholder={this.state.birth_date ? this.state.birth_date : "Birth Date" } onChange={(event) => {this.setState({birth_date: event.target.value})}} />
+  // </Form.Field>
+
+
   render () {
+    console.log(this.state.search_word);
     const { activeItem } = this.state
-    const stateOptions = [{text: 'Module 1'}, {text: 'Module 2'}];
+    const stateOptions = [{text: 'Module 1'}, {text: 'Module 2'}, {text: 'Module 3'}, {text: 'Module 4'}, {text: 'Module 5'}, {text: 'Data Science'}];
 
     return (
       <div className="background-nav">
       <Header as='h1'textAlign='center'>Projects</Header>
       <Menu pointing secondary>
-        <Input focus placeholder='Search...' />
-        <Dropdown placeholder='State' search selection options={stateOptions} />
+        <Input focus placeholder='Search...' onChange={(event) => {this.setState({search_word: event.target.value})}}/>
+        <Dropdown placeholder='State' search selection options={stateOptions}  />
         <Menu.Menu position='right'>
         <Menu.Item
           name='Add Project'
